d88a798e95dcd074f3ba03c238d1524d151e42fe
Rafael <rafaelyokoyama925@gmail.com>
2020-06-24 15:07:28 -0300

    criado a interface do app
    criado a interface do app,adicionado imagens

---
+++ Cara_ou_Coroa/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:Cara_ou_Coroa/Jogar.dart';
+import 'package:flutter/material.dart';
+import 'package:Cara_ou_Coroa/Jogar.dart';
+
+
+
+void main () => runApp(MaterialApp(
+  home: Jogar() , 
+  debugShowCheckedModeBanner: false,
+
+
+
+
+));
+
+
+
+
+
