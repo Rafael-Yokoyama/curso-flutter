9104b11cd789795909bf3953e1e28ccca83651b1
Rafael <rafaelyokoyama925@gmail.com>
2020-07-06 11:48:55 -0300

    recuperar videos
    recuperar vídeos

---
+++ youtube/lib/Model/Video.dart
@@ -0,0 +1,15 @@
+
+class Video {
+
+  String id;
+  String titulo;
+  String descricao;
+  String imagem;
+  String canal;
+
+  Video({this.id, this.titulo, this.descricao, this.imagem, this.canal});
+
+
+
+
+}
--- youtube/lib/pasta/Inicio.dart
+++ youtube/lib/pasta/Inicio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/Api.dart';
 
 
 
@@ -6,10 +7,14 @@ class Inicio extends StatefulWidget {
   @override
   _InicioState createState() => _InicioState();
 }
-
 class _InicioState extends State<Inicio> {
+ 
   @override
   Widget build(BuildContext context) {
+
+  Api api = Api();
+  api.pesquisar("");
+
     return Container(
       child: Center(
         child:Text("Início",
