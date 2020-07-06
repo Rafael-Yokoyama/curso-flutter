6488b0f427028e1506e8dff16d5a8084fb314483
Rafael <rafaelyokoyama925@gmail.com>
2020-07-06 10:06:12 -0300

    criada a tela de navegação inferior
    foi criada a navegação dos icones inferirores

--- youtube/lib/Home.dart
+++ youtube/lib/Home.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/pasta/Biblioteca.dart';
+import 'package:youtube/pasta/EmAlta.dart';
+import 'package:youtube/pasta/Inicio.dart';
+import 'package:youtube/pasta/Inscricao.dart';
 
 class Home extends StatefulWidget {
+
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+    int  _indiceAtual = 0 ;
+
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [
+
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+   
+
+
+    ];
+
+
+    
     return Scaffold(
       appBar:AppBar(
         iconTheme: IconThemeData(
@@ -62,11 +84,55 @@ class _HomeState extends State<Home> {
 
 
       ) ,
-      body: Container(
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex:  _indiceAtual ,
+        onTap: (indice){
+          setState(() {
+             _indiceAtual = indice ;
+            
+          });
 
 
 
-      ),
+        },
+
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Início"),
+            icon: Icon(Icons.home),
+           ),
+            BottomNavigationBarItem(
+            title: Text("Em alta"),
+            icon: Icon(Icons.whatshot),
+           ),
+            BottomNavigationBarItem(
+            title: Text("Incrições"),
+            icon: Icon(Icons.subscriptions),
+           ),
+            BottomNavigationBarItem(
+            title: Text("Biblioteca"),
+            icon: Icon(Icons.folder),
+           ),
+               
+               
+                              
+                          
+         
+
+
+
+
+        ]
+        
+        
+        
+        ),
+        
+  
+
 
 
     );
